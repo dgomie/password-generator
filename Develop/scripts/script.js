fff6526fbef49a42d8828a411a6bbe0bb8ad7057
Danny <danny.f.gomez@gmail.com>
2024-02-17 16:11:19 -0500

    Add for loop adding characters to password array

--- Develop/scripts/script.js
+++ Develop/scripts/script.js
@@ -1,6 +1,31 @@
 // Assignment code here
 function generatePassword() {
 
+  function randomChoice(array) {
+    var random = Math.floor(Math.random() * array.length);
+    return array[random];
+  }
+
+  function randomNumber(x) {
+    var randomNum = Math.floor(Math.random() * x);
+    return randomNum;
+  }
+  // Characters object
+  var characters = {
+    lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    upper: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    symbols: ['!', '#', '$', '%', '&', '(', ')', '*', '+'],
+}
+  var newPassword = [];
+
+  // Testing randomChoice function
+  var randomTest = randomChoice(characters.symbols);
+  console.log(randomTest);
+
+  randomTest = randomNumber(8);
+  console.log(randomTest);
+
   // Ask user for number of characters
   var numChars = prompt("How many characters in your password?");
   
@@ -18,26 +43,35 @@ function generatePassword() {
 
   // Ask if user wants lower case characters
   var lowerChar = window.confirm("Would you like lower case characters?\n (Click 'OK' for Yes or 'Cancel' for No)");
-
   // Ask if user wants upper case characters
   var upperChar = window.confirm("Would you like upper case characters?\n (Click 'OK' for Yes or 'Cancel' for No)");
-
   // Ask if user wants numeric characters
   var numericChar = window.confirm("Would you like numeric characters?\n (Click 'OK' for Yes or 'Cancel' for No)");
-
   // Ask if user wants special characters
   var specialChar = window.confirm("Would you like special characters?\n (Click 'OK' for Yes or 'Cancel' for No)");
+  
+  for (let i = numChars; i > 0;) {
+    if (lowerChar) {
+      newPassword.push(randomChoice(characters.lower));
+      i--;
+    } 
+    if (upperChar) {
+      newPassword.push(randomChoice(characters.upper));
+      i--;
+    } 
+    if (numericChar) {
+      newPassword.push(randomChoice(characters.numbers));
+      i--;
+    }
+    if (specialChar) {
+      newPassword.push(randomChoice(characters.symbols));
+      i--;
+    }
+  }
 
- 
-   
+  console.log(newPassword)
+}
 
-  var characters = {
-    lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-    upper: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-    numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-    symbols: ['!', '#', '$', '%', '&', '(', ')', '*', '+'],
-}
-}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
